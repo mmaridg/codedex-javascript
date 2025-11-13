6baa0db6e184e8a2c6fa96aa05d8c5efa437bde0
Mariana Dias <mmaridg@gmail.com>
2025-11-13 20:51:33 +0000

    Add package.json and update temperature.js

---
+++ temperature.js
@@ -0,0 +1,6 @@
+let fBrooklyn = 50;
+let cBrooklyn = (fBrooklyn - 32) / 1.8;
+
+console.log("The temperature in Celsius is: " + cBrooklyn);
+console.log(fBrooklyn + "°F is equal to " + cBrooklyn + "°C.");
+
